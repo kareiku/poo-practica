8e262ff8c81ffef0b4b8876ecc6cc9418ceb7491
kareiku <xaxvu7@gmail.com>
2024-09-26 00:33:32 +0200

    add matchmake class

---
+++ src/main/java/Matchmake.java
@@ -0,0 +1,17 @@
+public class Matchmake {
+    private final Player player1;
+    private final Player player2;
+
+    public Matchmake(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public Player getPlayer1() {
+        return player1;
+    }
+
+    public Player getPlayer2() {
+        return player2;
+    }
+}
