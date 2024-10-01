93be05eddfa761e39b8549ebae354c7bdeb74f3f
kareiku <xaxvu7@gmail.com>
2024-10-01 21:00:48 +0200

    grammar fix

--- src/main/java/project/SportManagementSystem.java
+++ src/main/java/project/SportManagementSystem.java
@@ -4,19 +4,19 @@ public class SportManagementSystem {
     public static void main(String[] args) {
         Manager manager = new Manager();
 
-        manager.input("run create Luisa");
-        manager.input("run score Luisa 4.5");
+        manager.input("create Luisa");
+        manager.input("score Luisa 4.5");
 
-        manager.input("run create Manuel");
-        manager.input("run score Manuel 2.7");
+        manager.input("create Manuel");
+        manager.input("score Manuel 2.7");
 
-        manager.input("run create Kurt");
-        manager.input("run score Kurt 4.0");
+        manager.input("create Kurt");
+        manager.input("score Kurt 4.0");
 
-        manager.input("run create Sofia");
-        manager.input("run score Sofia 3.8");
+        manager.input("create Sofia");
+        manager.input("score Sofia 3.8");
 
-        manager.input("run create Robert");
-        manager.input("run score Robert 3.8");
+        manager.input("create Robert");
+        manager.input("score Robert 3.8");
     }
 }
