8c10180a7b06e28a17725e6518404200725afc99
kareiku <xaxvu7@gmail.com>
2024-12-04 21:21:41 +0100

    Empty commit

--- src/main/java/org/example/views/commands/Command.java
+++ src/main/java/org/example/views/commands/Command.java
@@ -3,7 +3,14 @@ package org.example.views.commands;
 import org.example.controllers.Controller;
 
 public abstract class Command {
+    private final Controller controller;
+
     protected Command(Controller controller) {
+        this.controller = controller;
+    }
+
+    protected Controller controller() {
+        return controller;
     }
 
     public abstract void execute(String[] args);
