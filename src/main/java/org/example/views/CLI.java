f21a65f6c491e149fe3218b0c172afe12919f53e
kareiku <xaxvu7@gmail.com>
2024-12-05 10:51:30 +0100

    Empty commit

--- src/main/java/org/example/views/CLI.java
+++ src/main/java/org/example/views/CLI.java
@@ -2,22 +2,26 @@ package org.example.views;
 
 import org.example.controllers.CommandFactory;
 import org.example.utils.Console;
-import org.example.models.Message;
 import org.example.utils.Error;
 import org.example.views.commands.Command;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CLI {
     private final CommandFactory factory;
+    private final MessageView messageView;
 
-    public CLI() {
+    private CLI() {
         this.factory = new CommandFactory();
+        this.messageView = new MessageView();
     }
 
-    public void start() {
+    private void start() {
         String statement;
-        Console.getInstance().println(Message.WELCOME.getMessage());
+        this.messageView.println(Message.WELCOME);
         do {
-            Console.getInstance().print(Message.PROMT.getMessage());
+            this.messageView.println(Message.PROMT);
             statement = Console.getInstance().readLine();
             String commandName = statement.split("\\s+", 2)[0];
             String[] args = statement.split("\\s+", 2)[1].split((";")); // fixme possible ArrayOutOfBoundsException?
@@ -28,10 +32,35 @@ public class CLI {
                 Console.getInstance().println(Error.COMMAND_NOT_FOUND.getMessage());
             }
         } while (!statement.equalsIgnoreCase("exit"));
-        Console.getInstance().println(Message.BYE.getMessage());
+        this.messageView.println(Message.BYE);
     }
 
     public static void main(String[] args) {
         new CLI().start();
+    }
+}
+
+enum Message {
+    WELCOME, PROMT, BYE
+}
+
+class MessageView {
+    private final Map<Message, String> messages;
+
+    MessageView() {
+        this.messages = new HashMap<>();
+        this.associateMessages();
+    }
+
+    void println(Message message) {
+        Console.getInstance().println(messages.get(message));
+    }
+
+    private void associateMessages() {
+        this.messages.putIfAbsent(Message.WELCOME, "Welcome to the Sport's Management System.\n" +
+                "Write a command to start using the app.\n" +
+                "If unsure, use the \"help\" command.");
+        this.messages.putIfAbsent(Message.PROMT, "> ");
+        this.messages.putIfAbsent(Message.BYE, "Exiting the application...");
     }
 }
